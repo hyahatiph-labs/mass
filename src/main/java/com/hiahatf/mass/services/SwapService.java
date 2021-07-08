02f7528303684ed66d809ceea98593804051527d
reemuru <nigel.l.christian@gmail.com>
2021-07-08 16:05:23 -0400

    mass: add flow todos for swapping

--- src/main/java/com/hiahatf/mass/services/SwapService.java
+++ src/main/java/com/hiahatf/mass/services/SwapService.java
@@ -1,5 +1,18 @@
 package com.hiahatf.mass.services;
 
+import org.springframework.stereotype.Service;
+
+@Service("SwapService")
 public class SwapService {
     
+    // TODO: fetch quote, if not fulfilled
+
+    // TODO: verify inflight payment
+
+    // TODO: send monero
+
+    // TODO: if xmr rpc success
+    // settle hold invoice
+
+    // TODO: update quote to fulfilled
 }
