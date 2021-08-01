3f43a2a793d8c07bd1f308606f219b992d79d0f2
reemuru <nigel.l.christian@gmail.com>
2021-08-01 17:44:59 -0400

    monero: add rpc templates for multisig

--- src/main/java/org/hiahatf/mass/services/rpc/Monero.java
+++ src/main/java/org/hiahatf/mass/services/rpc/Monero.java
@@ -6,6 +6,31 @@ import com.google.common.collect.Lists;
 
 import org.hiahatf.mass.models.Constants;
 import org.hiahatf.mass.models.monero.Destination;
+import org.hiahatf.mass.models.monero.multisig.DescribeParameters;
+import org.hiahatf.mass.models.monero.multisig.DescribeRequest;
+import org.hiahatf.mass.models.monero.multisig.DescribeResponse;
+import org.hiahatf.mass.models.monero.multisig.ExportInfoRequest;
+import org.hiahatf.mass.models.monero.multisig.ExportInfoResponse;
+import org.hiahatf.mass.models.monero.multisig.FinalizeParameters;
+import org.hiahatf.mass.models.monero.multisig.FinalizeRequest;
+import org.hiahatf.mass.models.monero.multisig.FinalizeResponse;
+import org.hiahatf.mass.models.monero.multisig.ImportInfoParameters;
+import org.hiahatf.mass.models.monero.multisig.ImportInfoRequest;
+import org.hiahatf.mass.models.monero.multisig.ImportInfoResponse;
+import org.hiahatf.mass.models.monero.multisig.MakeParameters;
+import org.hiahatf.mass.models.monero.multisig.MakeRequest;
+import org.hiahatf.mass.models.monero.multisig.MakeResponse;
+import org.hiahatf.mass.models.monero.multisig.PrepareRequest;
+import org.hiahatf.mass.models.monero.multisig.PrepareResponse;
+import org.hiahatf.mass.models.monero.multisig.SignParameters;
+import org.hiahatf.mass.models.monero.multisig.SignRequest;
+import org.hiahatf.mass.models.monero.multisig.SignResponse;
+import org.hiahatf.mass.models.monero.multisig.SubmitParameters;
+import org.hiahatf.mass.models.monero.multisig.SubmitRequest;
+import org.hiahatf.mass.models.monero.multisig.SubmitResponse;
+import org.hiahatf.mass.models.monero.multisig.SweepAllParameters;
+import org.hiahatf.mass.models.monero.multisig.SweepAllRequest;
+import org.hiahatf.mass.models.monero.multisig.SweepAllResponse;
 import org.hiahatf.mass.models.monero.proof.GetReserveProofParameters;
 import org.hiahatf.mass.models.monero.proof.GetReserveProofRequest;
 import org.hiahatf.mass.models.monero.proof.GetReserveProofResponse;
@@ -76,8 +101,8 @@ public class Monero {
      * Spring WebFlux, run Monero Wallet RPC with the
      * --rpc-disable-login flag.
      * TODO: roll custom digest authentication support
-     * @param value
      * @param address
+     * @param amount
      * @return Mono<MoneroTransferResponse>
      */
     public Mono<TransferResponse> transfer(String address, Double amount) {
@@ -107,7 +132,7 @@ public class Monero {
      * Spring WebFlux, run Monero Wallet RPC with the
      * --rpc-disable-login flag.
      * TODO: roll custom digest authentication support
-     * @param value
+     * @param amount
      * @return Mono<GetReserveProofResponse>
      */
     public Mono<GetReserveProofResponse> getReserveProof(Double amount) {
@@ -133,7 +158,7 @@ public class Monero {
      * Spring WebFlux, run Monero Wallet RPC with the
      * --rpc-disable-login flag.
      * TODO: roll custom digest authentication support
-     * @param value
+     * @param filename
      * @return Mono<CreateWalletResponse>
      */
     public Mono<CreateWalletResponse> createWallet(String filename) {
@@ -158,7 +183,8 @@ public class Monero {
      * Spring WebFlux, run Monero Wallet RPC with the
      * --rpc-disable-login flag.
      * TODO: roll custom digest authentication support
-     * @param value
+     * @param state
+     * @param filename
      * @return Mono<WalletStateResponse>
      */
     public Mono<WalletStateResponse> controlWallet(WalletState state, 
@@ -181,6 +207,223 @@ public class Monero {
             .bodyValue(request)
             .retrieve()
             .bodyToMono(WalletStateResponse.class);
+    }
+
+    /**
+     * Make the Monero prepare_multisig RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @return Mono<PrepareResponse>
+     */
+    public Mono<PrepareResponse> prepareMultisig() {
+        // build request
+        PrepareRequest request = PrepareRequest.builder().build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(PrepareResponse.class);
+    }
+
+    /**
+     * Make the Monero make_multisig RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param multisigInfo
+     * @return Mono<MakeResponse>
+     */
+    public Mono<MakeResponse> makeMultisig(List<String> multisigInfo) {
+        // build request
+        MakeParameters parameters = MakeParameters.builder()
+            .multisig_info(multisigInfo).build();
+        MakeRequest request = MakeRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(MakeResponse.class);
+    }
+
+    /**
+     * Make the Monero finalize_multisig RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param multisigInfo
+     * @return Mono<FinalizeResponse>
+     */
+    public Mono<FinalizeResponse> finalizeMultisig(List<String> multisigInfo) {
+        // build request
+        FinalizeParameters parameters = FinalizeParameters.builder()
+            .multisig_info(multisigInfo).build();
+        FinalizeRequest request = FinalizeRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(FinalizeResponse.class);
+    }
+    
+    /**
+     * Make the Monero import_multisig_info RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param info
+     * @return Mono<ImportInfoResponse>
+     */
+    public Mono<ImportInfoResponse> importMultisigInfo(List<String> info) {
+        // build request
+        ImportInfoParameters parameters = ImportInfoParameters.builder()
+            .info(info).build();
+        ImportInfoRequest request = ImportInfoRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(ImportInfoResponse.class);
+    }
+
+    /**
+     * Make the Monero export_multisig_info RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @return Mono<ExportInfoResponse>
+     */
+    public Mono<ExportInfoResponse> exportMultisigInfo() {
+        // build request
+        ExportInfoRequest request = ExportInfoRequest.builder().build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(ExportInfoResponse.class);
+    }
+
+    /**
+     * Make the Monero sign_multisig RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param txDataHex
+     * @return Mono<SignResponse>
+     */
+    public Mono<SignResponse> signMultisig(String txDataHex) {
+        // build request
+        SignParameters parameters = SignParameters.builder()
+            .tx_data_hex(txDataHex).build();
+        SignRequest request = SignRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(SignResponse.class);
+    }
+
+    /**
+     * Make the Monero submit_multisig RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param txDataHex
+     * @return Mono<SubmitResponse>
+     */
+    public Mono<SubmitResponse> submitMultisig(String txDataHex) {
+        // build request
+        SubmitParameters parameters = SubmitParameters.builder()
+            .tx_data_hex(txDataHex).build();
+        SubmitRequest request = SubmitRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(SubmitResponse.class);
+    }
+
+    /**
+     * Make the Monero describe_transfer RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param mulltisigTxSet
+     * @return Mono<DescribeResponse>
+     */
+    public Mono<DescribeResponse> describeTransfer(String mulltisigTxSet) {
+        // build request
+        DescribeParameters parameters = DescribeParameters.builder()
+            .multisig_txset(mulltisigTxSet).build();
+        DescribeRequest request = DescribeRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(DescribeResponse.class);
+    }
+
+    /**
+     * Make the Monero sweep_all RPC call.
+     * Due to lack of digest authentication support in 
+     * Spring WebFlux, run Monero Wallet RPC with the
+     * --rpc-disable-login flag.
+     * TODO: roll custom digest authentication support
+     * @param address
+     * @return Mono<SweepAllResponse>
+     */
+    public Mono<SweepAllResponse> sweepAll(String address) {
+        // build request
+        SweepAllParameters parameters = SweepAllParameters.builder()
+            .address(address).build();
+        SweepAllRequest request = SweepAllRequest.builder()
+            .params(parameters).build();
+        // monero rpc web client
+        WebClient client = WebClient.builder().baseUrl(moneroHost).build();
+        return client.post()
+            .uri(uriBuilder -> uriBuilder
+            .path(Constants.JSON_RPC).build())
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(SweepAllResponse.class);
     }
 
 }
